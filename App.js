07121f51ca62b2aa8451e7614d3995c2635c387e
RyleyGG <ryleyg@delmarfans.com>
2022-05-26 16:35:42 -0400

    Basic answer view added.
    Basic answer view added to demonstrate fade between first and second views. Bug with return to initial fade not properly playing the fade in

--- App.js
+++ App.js
@@ -1,31 +1,84 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Animated, Text, View, StyleSheet, Button } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function App() {
-		/* main display props */
-		const [initialView, setInitialView] = React.useState(true);
+	/* main display props */
+	const [initialView, setInitialView] = React.useState('flex');
+  const initialFade = React.useRef(new Animated.Value(1)).current;
+	const [answerView, setAnswerView] = React.useState('none');
+	const answerFade = React.useRef(new Animated.Value(0)).current;
 
-		/* display of questions */
-    const [questionOpen, setQuestionOpen] = React.useState(false);
-    const [questionValue, setQuestionValue] = React.useState(null);
-    const [questions, setQuestions] = React.useState([
-      {label: 'Apple', value: 'apple'},
-      {label: 'Banana', value: 'banana'}
-    ])
+	/* display of questions */
+	const [questionOpen, setQuestionOpen] = React.useState(false);
+	const [questionValue, setQuestionValue] = React.useState(null);
+	const [questions, setQuestions] = React.useState([
+		{label: 'Apple', value: 'apple'},
+		{label: 'Banana', value: 'banana'}
+	])
+	
+	const submitQuestion = async () => {
+		console.log(questionValue);
+    Animated.timing(initialFade, {
+      toValue: 0,
+      duration: 1500
+    }).start(() => generateAnswer());
+	};
+	
+	const generateAnswer = async () => {
+		setInitialView('none');
+		setAnswerView('flex');
+    Animated.timing(answerFade, {
+      toValue: 1,
+      duration: 1500
+    }).start();
+	};
+
+	const returnToInitial = async () => {
+    Animated.timing(answerFade, {
+      toValue: 0,
+      duration: 1500
+    }).start(() => setAnswerView('none'));
+
+    Animated.timing(initialFade, {
+      toValue: 1,
+      duration: 1500
+    }).start(() => setInitialView('flex'));
+
+	};
 
   return (
-    <View style={styles.container} display={initialView}>
-      <Text>Select your question from the dropdown below<br />and confirm when you're ready for your <b>fate</b></Text>
-			<DropDownPicker
-			open={questionOpen}
-			value={questionValue}
-			items={questions}
-			setOpen={setQuestionOpen}
-			setValue={setQuestionValue}
-			setItems={setQuestions}
-			/>
-    </View>
+		<View style={styles.container}>
+			<Animated.View style={[styles.container, {opacity: initialFade, display: initialView}]}>
+				<Text>Select your question from the dropdown below<br />and confirm when you're ready for your <b>fate</b></Text>
+				<DropDownPicker
+				open={questionOpen}
+				value={questionValue}
+				items={questions}
+				setOpen={setQuestionOpen}
+				setValue={setQuestionValue}
+				setItems={setQuestions}
+				/>
+				
+				<Button
+				onPress={submitQuestion}
+				title="Confirm"
+				color="#841584"
+				accessibilityLabel="Button for confirming question selection"
+				/>
+			</Animated.View>
+
+			<Animated.View style={[styles.container, {opacity: answerFade, display: answerView}]}>
+				<Text>This is the answer view. Nice...</Text>
+				
+				<Button
+				onPress={returnToInitial}
+				title="Start over"
+				color="#841584"
+				accessibilityLabel="Button for returning to question selection"
+				/>
+			</Animated.View>
+		</View>
   );
 }
 
