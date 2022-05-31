bcf205f721d698d9f3c3d899f41049103c261dac
RyleyGG <griffithryley2000@gmail.com>
2022-05-31 18:36:44 -0400

    Set up detailed stat view (local implemented, global groundwork added)

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
-import { Animated, Text, View, StyleSheet, Button } from 'react-native';
+import { Animated, Text, View, StyleSheet, Button, Dimensions } from 'react-native';
 import { Dropdown } from 'react-native-element-dropdown';
+import {
+    BarChart,
+  } from "react-native-chart-kit";
 
 export default function App() {
 	/* main display props */
@@ -8,24 +11,28 @@ export default function App() {
   const initialFade = React.useRef(new Animated.Value(1)).current;
 	const [answerView, setAnswerView] = React.useState('none');
 	const answerFade = React.useRef(new Animated.Value(0)).current;
+	const [globalStatView, setGlobalStatView] = React.useState('none');
+	const globalStatFade = React.useRef(new Animated.Value(0)).current;
 
 	/* question/answer props */
 	const [questionsFocused, setQuestionsFocused] = React.useState(false);
 	const [questionValue, setQuestionValue] = React.useState(null);
 	const [selectedAnswer, setSelectedAnswer] = React.useState(null);
 	const [answerDict, setAnswerDict] = React.useState({});
-	const [answerCount, setAnswerCount] = React.useState(1);
+	const [answerCount, setAnswerCount] = React.useState(100);
 	const [badAnswerCount, setBadAnswerCount] = React.useState(false);
+	const [largestAnswerCount, setLargestAnswerCount] = React.useState(0);
 	const answers = [
 		'Yes',
+		'Absolutely!',
+		'Without a doubt',
 		'No',
 		'Uh... absolutely not',
 		'I\'ll give it a 50/50 chance',
 		'Maybe',
-		'Crystal ball is foggy, check back later',
-		'Internet tubes are clogged, check back later',
+		'Crystal ball broken, sorry!',
 		'Sure, why not?',
-		'Why would you even bother asking me that?'
+		'Why bother asking?'
 	];
 	
 	const questions  = [
@@ -44,8 +51,54 @@ export default function App() {
 		{label: 'Should I get another dog?', value: 'new_dog'},
 		{label: 'Will I ever go to jail?', value: 'jail'},
 		{label: 'Will I ever break out of jail?', value: 'jail_break'}
-	]
+	];
+
+	const [barData, setBarData] = React.useState({labels: [], datasets: [{data: []}]});
+
+
+	const chartConfig = {
+		backgroundGradientFrom: "#1E2923",
+		backgroundGradientTo: "#1E2923",
+		decimalPlaces: 0,
+		color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+		labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+	};
 	
+	/* APP TRAVERSAL FUNCTIONS */
+	const viewReset = async () => {
+		setInitialView('none');
+		setAnswerView('none');
+		setGlobalStatView('none');
+		setQuestionValue(null);
+		setAnswerDict({});
+	};
+
+	const toInitial = async (fadeObj) => {
+    Animated.timing(fadeObj, {
+      toValue: 0,
+      duration: 1000
+    }).start(() => {
+			viewReset();
+			setInitialView('flex');
+			Animated.timing(initialFade, {
+				toValue: 1,
+				duration: 1000
+			}).start()});
+	};
+
+	const toGlobalStats = async (fadeObj) => {
+        Animated.timing(fadeObj, {
+          toValue: 0,
+          duration: 1000
+        }).start(() => {
+                viewReset();
+                setGlobalStatView('flex');
+                Animated.timing(globalStatFade, {
+                    toValue: 1,
+                    duration: 1000
+                }).start()});
+	};
+
 	const submitQuestion = async () => {
     Animated.timing(initialFade, {
       toValue: 0,
@@ -60,6 +113,8 @@ export default function App() {
 			}).start(() => saveAnswers())});
 	};
 	
+	
+	/* MAIN FUNCTIONS */
 	const generateAnswers = async () => {
 		let iter = 0;
 
@@ -75,27 +130,18 @@ export default function App() {
 
 			iter++;
 		}
+
+		/* once answers are generated, save in bar data in appropriate format */
+		setBarData({labels: Object.keys(answerDict), datasets: [{data: Object.values(answerDict)}]});
+		setLargestAnswerCount(Object.values(answerDict).reduce(function(a, b) {
+				return Math.max(a, b);
+		}, -Infinity));
 	};
 
 	const saveAnswers = async () => {
 
 	};
 
-	const returnToInitial = async () => {
-    Animated.timing(answerFade, {
-      toValue: 0,
-      duration: 1000
-    }).start(() => {
-			setInitialView('flex');
-			setAnswerView('none');
-			setQuestionValue(null);
-			setAnswerDict({});
-			Animated.timing(initialFade, {
-				toValue: 1,
-				duration: 1000
-			}).start()});
-
-	};
 
 	useEffect(async () => {
 		if (answerCount < 1 || answerCount > 1000) {
@@ -106,13 +152,6 @@ export default function App() {
 		}
 	}, [answerCount]);
 
-	const viewLocalStats = async () => {
-
-	};
-
-	const viewGlobalStats = async () => {
-
-	};
 
   return (
 		<View style={styles.container}>
@@ -158,29 +197,58 @@ export default function App() {
 
 			{/* answer display */}
 			<Animated.View style={[styles.container, {opacity: answerFade, display: answerView}]}>
+				{/* {Object.keys(answerDict).map((answer) => (
+					<Text><b><i>{answer}</i></b> was rolled {answerDict[answer]} times</Text>
+				))}<br /><br /> */}
+
+				<BarChart
+				data={barData}
+				width={Dimensions.get("window").width * 0.85}
+				height={500}
+				yAxisInterval = {1}
+				fromZero = {true}
+				segments = {largestAnswerCount}
+				chartConfig={chartConfig}
+				/><br /><br />
+				
+      	<View style={styles.buttonView}>
+					<Button
+					onPress={() => toInitial(answerFade)}
+					title="Start over"
+					color="#841584"
+					accessibilityLabel="Button for returning to question selection"
+					/>
+
+					<Button
+					onPress={() => toGlobalStats(answerFade)}
+					title="View global stats"
+					color="#841584"
+					accessibilityLabel="Button for displaying global answer stats"
+					/>
+				</View>
+			</Animated.View>
+
+			{/* global stat display */}
+			<Animated.View style={[styles.container, {opacity: globalStatFade, display: globalStatView}]}>
 				{Object.keys(answerDict).map((answer) => (
 					<Text><b><i>{answer}</i></b> was rolled {answerDict[answer]} times</Text>
 				))}<br /><br />
 				
+				<BarChart
+				data={barData}
+				width={Dimensions.get("window").width * 0.85}
+				height={500}
+				yAxisInterval = {1}
+				fromZero = {true}
+				segments = {largestAnswerCount}
+				chartConfig={chartConfig}
+				/>
+        
 				<Button
-				onPress={returnToInitial}
+				onPress={() => toInitial(globalStatFade)}
 				title="Start over"
 				color="#841584"
 				accessibilityLabel="Button for returning to question selection"
-				/>
-
-				<Button
-				onPress={viewLocalStats}
-				title="View local stats"
-				color="#841584"
-				accessibilityLabel="Button for displaying local answer stats"
-				/>
-
-				<Button
-				onPress={viewGlobalStats}
-				title="View global stats"
-				color="#841584"
-				accessibilityLabel="Button for displaying global answer stats"
 				/>
 			</Animated.View>
 		</View>
@@ -194,6 +262,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     textAlign: 'center'
+  },
+  buttonView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '50%',
+    padding: 10,
   },
 	placeholderStyle: {
 		fontSize: 13,
